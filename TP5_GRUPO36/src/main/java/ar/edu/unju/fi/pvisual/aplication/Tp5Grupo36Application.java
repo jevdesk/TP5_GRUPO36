35330f0c5e8f02ade99f2694437147ea6cccba93
desc <desc@gato-pc>
2022-05-02 17:47:13 -0300

    agregando avances del tp4

---
+++ TP5_GRUPO36/src/main/java/ar/edu/unju/fi/pvisual/aplication/Tp5Grupo36Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo36Application.class, args);
+	}
+
+}
---
+++ TP5_GRUPO36/src/main/java/ar/edu/unju/fi/pvisual/controller/MainController.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.fi.pvisual.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String getIndex() {		
+		return "index";
+	}
+	
+	@GetMapping("/agenda")
+	public String getAgendaPage() {		
+		return "agenda";
+	}
+
+	@GetMapping("/beneficios")
+	public String getBeneficiosPage() {		
+		return "beneficios";
+	}
+	
+	@GetMapping("/contacto")
+	public String getContactoPage() {		
+		return "contacto";
+	}
+	@GetMapping("/design")
+	public String getDesingPage() {		
+		return "design";
+	}
+
+}
